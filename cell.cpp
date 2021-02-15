7c7ba9c970c48e12004c2d9306042183f0906a16
Alexander Larin <scalar438@gmail.com>
2021-02-15 22:09:55 +0300

    Fix compile errors && run clang-format

--- cell.cpp
+++ cell.cpp
@@ -6,9 +6,9 @@ namespace
 // Calculate number of enabled bit.
 constexpr int get_bit_number(uint32_t val)
 {
-	static const int MultiplyDeBruijnBitPosition[32] = {0,  1,  28, 2,  29, 14, 24, 3,  30, 22, 20,
-	                                                    15, 25, 17, 4,  8,  31, 27, 13, 23, 21, 19,
-	                                                    16, 7,  26, 12, 18, 6,  11, 5,  10, 9};
+	constexpr int MultiplyDeBruijnBitPosition[32] = {0,  1,  28, 2,  29, 14, 24, 3,  30, 22, 20,
+	                                                 15, 25, 17, 4,  8,  31, 27, 13, 23, 21, 19,
+	                                                 16, 7,  26, 12, 18, 6,  11, 5,  10, 9};
 	return MultiplyDeBruijnBitPosition[(val * 0x077CB531U) >> 27u];
 }
 
--- row_solver.cpp
+++ row_solver.cpp
@@ -4,10 +4,10 @@
 std::vector<int> adjust(std::vector<Cell> &cells, std::vector<std::pair<int, int>> &block_list)
 {
 	const size_t n = cells.size();
-if( n == 0) return {};
+	if (n == 0) return {};
 
 	const int max_colors = cells[0].max_colors();
-	const size_t k = block_list.size();
+	const size_t k       = block_list.size();
 
 	// can_place_prifix[i][j] == true if we can place first j blocks at the first i cells
 	// can_place_suffix - same, but for last j blocks and last n - i cells
@@ -19,36 +19,36 @@ if( n == 0) return {};
 		prefix.push_back(true);
 
 		size_t last_non_empty = std::string::npos;
-		bool prefix_possible = true;
-		for(size_t i = 0; i < n; ++i)
+		bool prefix_possible  = true;
+		for (size_t i = 0; i < n; ++i)
 		{
-			if(!cells[i].is_color_possible(0))
+			if (!cells[i].is_color_possible(0))
 			{
 				prefix_possible = false;
-				last_non_empty = i;
+				last_non_empty  = i;
 			}
 			prefix.push_back(prefix_possible);
 		}
-		if(last_non_empty == std::string::npos)
+		if (last_non_empty == std::string::npos)
 		{
-			suffix = std::vector<bool>(n + 1, false);
+			suffix    = std::vector<bool>(n + 1, false);
 			suffix[n] = true;
 		}
 		else
 		{
 			suffix.reserve(n + 1);
-			for(size_t i = 0; i <= last_non_empty; ++i) suffix.push_back(false);
-			for(size_t i = last_non_empty + 1; i <= n; ++i) suffix.push_back(true);
+			for (size_t i = 0; i <= last_non_empty; ++i)
+				suffix.push_back(false);
+			for (size_t i = last_non_empty + 1; i <= n; ++i)
+				suffix.push_back(true);
 		}
 		can_place_prefix.emplace_back(std::move(prefix));
 		can_place_suffix.emplace_back(std::move(suffix));
 	}
 
-	for(size_t i = 0; i != n; ++i)
+	for (size_t i = 0; i != n; ++i)
 	{
-		for(int i = 0; i <= max_colors; ++i)
-		{
-			
-		}
+		for (int i = 0; i <= max_colors; ++i)
+		{}
 	}
 }
