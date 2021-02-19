331baffffa036df58f5b5710424b1218c6a73273
Alexander Larin <scalar438@gmail.com>
2021-02-19 13:58:32 +0300

    Calculate placeability, for all blocks (not tested)

--- row_solver.cpp
+++ row_solver.cpp
@@ -12,36 +12,54 @@ void calc_blocks_placeability(const TCell &cells, const TBlock &blocks, std::vec
 
 	res.resize(k + 1);
 	res[0].resize(n + 1);
-	res[0][0]         = true;
+	res[0][0]      = true;
 	bool cur_value = true;
 	for (size_t i = 0; i != n; ++i)
 	{
 		if (cur_value && !cells[i].is_color_possible(0)) cur_value = false;
 		res[0][i + 1] = cur_value;
 	}
+	for (size_t i = 0; i != k; ++i)
+	{
+		res[i + 1].resize(n + 1);
+		res[i + 1][0] = false;
+
+		int current_allowed_count = 0;
+
+		for (int j = 0; j < n; ++j)
+		{
+			if (cells[j].is_color_possible(blocks[i].first))
+				current_allowed_count += 1;
+			else
+				current_allowed_count = 0;
+			res[i + 1][j + 1] =
+			    current_allowed_count >= blocks[i].second && res[i][j - blocks[i].second];
+		}
+	}
 }
 
-std::vector<int> adjust(std::vector<Cell> &cells, std::vector<std::pair<int, int>> &block_list)
+std::vector<int> adjust(std::vector<Cell> &cells, std::vector<std::pair<int, int>> &blocks)
 {
 	const size_t n = cells.size();
 	std::vector<int> res;
 	if (n == 0) return res;
 
 	const int max_colors = cells[0].max_colors();
-	const size_t k       = block_list.size();
 
 	// can_place_prifix[i][j] == true if we can place first i blocks at the first j cells
 	// can_place_suffix - same, but for last i blocks and last n - j cells
 	std::vector<std::vector<bool>> can_place_prefix, can_place_suffix;
 
-	calc_blocks_placeability(cells, block_list, can_place_prefix);
+	calc_blocks_placeability(cells, blocks, can_place_prefix);
 	{
 		std::vector<ReversedVector<bool>> can_place_suffix_rev;
-		for(auto &vec : can_place_suffix) can_place_suffix_rev.push_back(ReversedVector(vec));
-		calc_blocks_placeability(ReversedVector(cells), ReversedVector(block_list), can_place_suffix_rev);
+		for (auto &vec : can_place_suffix)
+			can_place_suffix_rev.push_back(ReversedVector(vec));
+		calc_blocks_placeability(ReversedVector(cells), ReversedVector(blocks),
+		                         can_place_suffix_rev);
 	}
 
-	calc_blocks_placeability(ReversedVector(cells), ReversedVector(block_list), can_place_suffix);
+	calc_blocks_placeability(ReversedVector(cells), ReversedVector(blocks), can_place_suffix);
 
 	for (size_t i = 0; i != n; ++i)
 	{
--- row_solver.hpp
+++ row_solver.hpp
@@ -6,4 +6,4 @@
 /// @param cells      - row, vector of cells with current state
 /// @param block_list - vector of pair (color number, block length)
 /// @return  number of cells with changed state
-std::vector<int> adjust(std::vector<Cell> &cells, std::vector<std::pair<int, int>> &block_list);
+std::vector<int> adjust(std::vector<Cell> &cells, std::vector<std::pair<int, int>> &blocks);
