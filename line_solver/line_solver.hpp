54a546963c687b9cb4a02b139860d89c4ca0216e
Alexander Larin <scalar438@gmail.com>
2024-03-23 10:58:11 +0300

    Update line_solver.hpp
    Fix comment

--- line_solver/line_solver.hpp
+++ line_solver/line_solver.hpp
@@ -3,8 +3,8 @@
 #include "cell.hpp"
 #include <vector>
 
-/// Calculate puzzle for one row.
-/// @param cells      - row, vector of cells with current state
+/// Reveal as much as possible about one line
+/// @param cells      - row, vector of cells with the current state
 /// @param block_list - vector of pair (color number, block length)
-/// @return cells indexes with changed state
+/// @return cells indexes with the changed state
 std::vector<size_t> calculate_line(std::vector<Cell> &cells, const std::vector<Block> &blocks);
